e208cee33362481800d58db9675ef1becab776b1
Balai Mikheeva <balaishka@yandex.ru>
2022-02-21 11:58:25 +0300

    fix: исправить ошибки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,57 +1,50 @@
-let page = document.querySelector('.page');
-let edit = page.querySelector('.profile__edit-btn');
-let popup = page.querySelector('.popup');
-let close = popup.querySelector('.popup__close-btn');
-let save = popup.querySelector('.edit__save-btn');
+// Находим нужные кнопки и блоки
+let page = document.querySelector(".page");
+let edit = page.querySelector(".profile__edit-btn");
+let popup = page.querySelector(".popup");
+let close = popup.querySelector(".popup__close-btn");
+let formElement = popup.querySelector(".edit");
 
-function popupOpen() {
-  popup.classList.add('popup_opened');
+// Находим значения name и about
+let profileName = page.querySelector(".profile__name");
+let profileAbout = page.querySelector(".profile__about");
 
-  let profileName = page.querySelector('.profile__name');
-  let profileAbout = page.querySelector('.profile__about');
+// Находим текстовые поля name и about в форме
+let editName = formElement.querySelector(".edit__text_type_name");
+let editAbout = formElement.querySelector(".edit__text_type_about");
 
-  let formElement = popup.querySelector('.edit');
-  let editName = formElement.querySelector('.edit__text_type_name');
-  let editAbout = formElement.querySelector('.edit__text_type_about');
+// Открыть попап
+function openPopup() {
+  popup.classList.add("popup_opened");
 
+  // Вставляем в текстовые поля значения из профиля
   editName.value = profileName.textContent;
   editAbout.value = profileAbout.textContent;
 }
 
-function popupClose() {
-  popup.classList.remove('popup_opened');
+// Закрыть попап
+function closePopup() {
+  popup.classList.remove("popup_opened");
 }
 
-function popupSave() {
-  let formElement = popup.querySelector('.edit');
-  let editName = formElement.querySelector('.edit__text_type_name');
-  let editAbout = formElement.querySelector('.edit__text_type_about');
+// Обработчик «отправки» формы
+function formSubmitHandler(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 
-  // Обработчик «отправки» формы
-  function formSubmitHandler (evt) {
-      evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Получите значение полей editName и editAbout из свойства value
+  let newName = editName.value;
+  let newAbout = editAbout.value;
 
-      // Получите значение полей editName и editAbout из свойства value
-      let newName = editName.value;
-      let newAbout = editAbout.value;
+  // Вставьте новые значения с помощью textContent
+  profileName.textContent = newName;
+  profileAbout.textContent = newAbout;
 
-      // Выберите элементы, куда должны быть вставлены значения полей
-      let profileName = page.querySelector('.profile__name');
-      let profileAbout = page.querySelector('.profile__about');
-
-      // Вставьте новые значения с помощью textContent
-      profileName.textContent = newName;
-      profileAbout.textContent = newAbout;
-  }
-
-  // Прикрепляем обработчик к форме:
-  formElement.addEventListener('submit', formSubmitHandler);
-
-  popupClose();
+  closePopup();
 }
 
-edit.addEventListener('click', popupOpen);
+// Прикрепляем обработчик к форме:
+formElement.addEventListener("submit", formSubmitHandler);
 
-close.addEventListener('click', popupClose);
+edit.addEventListener("click", openPopup);
 
-save.addEventListener('click', popupSave);
+close.addEventListener("click", closePopup);
