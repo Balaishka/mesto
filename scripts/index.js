e3286efd889cdadb0d90966705421e955e7990fb
Balai Mikheeva <balaishka@yandex.ru>
2022-03-19 15:27:49 +0300

    feat: сделать рендеринг фотографий из массива

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,51 @@
+// Массив карточек
+const photos = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 // Находим нужные кнопки и блоки
-let page = document.querySelector(".page");
-let edit = page.querySelector(".profile__edit-btn");
-let popup = page.querySelector(".popup");
-let close = popup.querySelector(".popup__close-btn");
-let formElement = popup.querySelector(".edit");
+const page = document.querySelector(".page");
+const edit = page.querySelector(".profile__edit-btn");
+const popup = page.querySelector(".popup");
+const close = popup.querySelector(".popup__close-btn");
+const formElement = popup.querySelector(".edit");
+
+// Находим нужные кнопки и значения для фотографий
+const photoList = document.querySelector(".photos__list");
+
+// Рендеринг фотографий из массива на страницу
+function renderPhotos(photo) {
+  const photoCard = document.querySelector(".photo-template").content.firstElementChild.cloneNode(true); // Клонируем photo-template
+
+  photoCard.querySelector(".photo__name").textContent = photo.name; // Добавляем имя карточки из массива
+  photoCard.querySelector(".photo__img").src = photo.link; // Добавляем ссылку на картинку из массива
+  photoCard.querySelector(".photo__img").alt = photo.name; // Добавляем alt картинки из массива
+
+  photoList.append(photoCard); // Добавляем карточку
+}
 
 // Находим значения name и about
 let profileName = page.querySelector(".profile__name");
@@ -48,3 +90,6 @@ formElement.addEventListener("submit", formSubmitHandler);
 edit.addEventListener("click", openPopup);
 
 close.addEventListener("click", closePopup);
+
+// Запускаем рендеринг фотографий
+photos.map(renderPhotos);
