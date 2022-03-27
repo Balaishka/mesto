04efe5869d6bd3bb9a5d00d0cee02ac78bfe3ddc
Balai Mikheeva <balaishka@yandex.ru>
2022-03-27 21:44:01 +0300

    feat: добавить валидацию, закрытие на esc и оверлей

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const page = document.querySelector(".page");
 const buttonEdit = page.querySelector(".profile__edit-btn");
 const popupEdit = page.querySelector(".popup_name_edit");
 const formElementEdit = popupEdit.querySelector(".form_edit");
+const buttonFormEdit = formElementEdit.querySelector(".form__btn_type_edit");
 
 // Находим значения name и about профиля
 const profileName = page.querySelector(".profile__name");
@@ -12,16 +13,27 @@ const profileAbout = page.querySelector(".profile__about");
 const nameEdit = formElementEdit.querySelector(".form__text_type_name");
 const aboutEdit = formElementEdit.querySelector(".form__text_type_about");
 
+// Находим текстовые поля ошибок name и about в форме профиля
+const nameError = formElementEdit.querySelector(".name-input-error");
+const aboutError = formElementEdit.querySelector(".about-input-error");
+
 // Находим нужные кнопки и значения фотографий
 const photoList = document.querySelector(".photos__list");
 const popupAddPhoto = document.querySelector(".popup_name_add-photo");
 const popupPicture = document.querySelector(".popup_name_picture");
 const buttonAdd = document.querySelector(".profile__add-btn");
 const formElementAddPhoto = popupAddPhoto.querySelector(".form_add-photo");
+const buttonFormAddPhoto = formElementAddPhoto.querySelector(
+  ".form__btn_type_add-photo"
+);
 
 // Находим текстовые поля title и link в форме фотографий
 const titleAdd = formElementAddPhoto.querySelector(".form__text_type_title");
 const linkAdd = formElementAddPhoto.querySelector(".form__text_type_link");
+
+// Находим текстовые поля ошибок title и link в форме фотографий
+const titleError = formElementAddPhoto.querySelector(".title-input-error");
+const linkError = formElementAddPhoto.querySelector(".link-input-error");
 
 // Находим поля title и Img в попапе фотографий
 const pictureTitle = popupPicture.querySelector(".popup__picture-title");
@@ -36,12 +48,30 @@ const btnClosePicture = popupPicture.querySelector(
   ".popup__close-btn_name_picture"
 );
 
-function openPopup(popup) {
-  popup.classList.add("popup_opened");
-}
-
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
+
+  document.removeEventListener("keydown", closePopupEsc);
+}
+
+function closePopupOverlay(event) {
+  if (event.currentTarget === event.target) {
+    closePopup(event.currentTarget);
+  }
+}
+
+function closePopupEsc(event) {
+  if (event.key === "Escape") {
+    const popup = document.querySelector(".popup_opened");
+
+    closePopup(popup);
+  }
+}
+
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+
+  document.addEventListener("keydown", closePopupEsc);
 }
 
 function deletePhoto(event) {
@@ -75,8 +105,12 @@ function createPhoto(photo) {
   photoCard.querySelector(".photo__img").src = photo.link; // Добавляем ссылку на картинку из массива
   photoCard.querySelector(".photo__img").alt = photo.name; // Добавляем alt картинки из массива
 
-  photoCard.querySelector(".photo__delete-btn").addEventListener("click", deletePhoto);
-  photoCard.querySelector(".photo__like-btn").addEventListener("click", likePhoto);
+  photoCard
+    .querySelector(".photo__delete-btn")
+    .addEventListener("click", deletePhoto);
+  photoCard
+    .querySelector(".photo__like-btn")
+    .addEventListener("click", likePhoto);
   photoCard.querySelector(".photo__img").addEventListener("click", openPhoto);
 
   return photoCard;
@@ -93,6 +127,15 @@ function openPropfilePopup() {
   nameEdit.value = profileName.textContent;
   aboutEdit.value = profileAbout.textContent;
 
+  nameEdit.classList.remove("form__text_invalid");
+  aboutEdit.classList.remove("form__text_invalid");
+
+  nameError.textContent = "";
+  aboutError.textContent = "";
+
+  buttonFormEdit.classList.remove("form__btn_disabled");
+  buttonFormEdit.removeAttribute("disabled", "");
+
   openPopup(popupEdit);
 }
 
@@ -101,13 +144,20 @@ function openAddPhotoPopup() {
   titleAdd.value = "";
   linkAdd.value = "";
 
+  titleAdd.classList.remove("form__text_invalid");
+  linkAdd.classList.remove("form__text_invalid");
+
+  titleError.textContent = "";
+  linkError.textContent = "";
+
+  buttonFormAddPhoto.classList.add("form__btn_disabled");
+  buttonFormAddPhoto.setAttribute("disabled", "");
+
   openPopup(popupAddPhoto);
 }
 
 // Обработчик «отправки» формы добавления карточки
 function formSubmitHandlerCreate(event) {
-  event.preventDefault(); // Эта строчка отменяет стандартную отправку формы
-
   // Создаем объект с введенной информацией
   const photo = {
     name: titleAdd.value,
@@ -121,8 +171,6 @@ function formSubmitHandlerCreate(event) {
 
 // Обработчик «отправки» формы редактирования
 function formSubmitHandlerEdit(event) {
-  event.preventDefault(); // Эта строчка отменяет стандартную отправку формы
-
   // Получаем значение полей nameEdit и aboutEdit из свойства value
   const newName = nameEdit.value;
   const newAbout = aboutEdit.value;
@@ -146,6 +194,8 @@ btnCloseEdit.addEventListener("click", () => {
   closePopup(popupEdit);
 });
 
+popupEdit.addEventListener("click", closePopupOverlay); // Закрываем попап редактирования при клике на оверлей
+
 formElementAddPhoto.addEventListener("submit", formSubmitHandlerCreate); // Прикрепляем обработчик к форме добавления фотографии
 
 // Открываем попап добавления фотографии
@@ -156,7 +206,11 @@ btnClosePhoto.addEventListener("click", () => {
   closePopup(popupAddPhoto);
 });
 
+popupAddPhoto.addEventListener("click", closePopupOverlay); // Закрываем попап добавления фотографии при клике на оверлей
+
 // Закрываем попап просмотра фотографии
 btnClosePicture.addEventListener("click", () => {
   closePopup(popupPicture);
 });
+
+popupPicture.addEventListener("click", closePopupOverlay); // Закрываем попап просмотра фотографии при клике на оверлей
