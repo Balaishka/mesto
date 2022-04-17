b0e14ca4e4a7d90e949f2cf6dc74ec5aeb597300
Balai Mikheeva <balaishka@yandex.ru>
2022-04-17 20:45:42 +0300

    feat: create class FormValidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,52 +1,80 @@
-import { toggleButtonDisabled, checkValid } from "./index.js";
-
 export class FormValidator {
-    constructor(form, formElement) {
-        this._form = form;
-        this._formElement = formElement;
+  constructor(form, formElement) {
+    this._form = form;
+    this._formElement = formElement;
+  }
+
+  // Показываем текст ошибки
+  _showInputError = (input, errorMessage) => {
+    const errorElement = this._formElement.querySelector(`.${input.id}-error`);
+
+    input.classList.add(this._form.inputErrorClass);
+    errorElement.textContent = errorMessage;
+  };
+
+  // Убираем текст ошибки
+  _hideInputError = (input) => {
+    const errorElement = this._formElement.querySelector(`.${input.id}-error`);
+
+    input.classList.remove(this._form.inputErrorClass);
+    errorElement.textContent = "";
+  };
+
+  // Проверяем инпуты на валидность
+  _checkValid = (input) => {
+    if (!input.validity.valid) {
+      this._showInputError(input, input.validationMessage);
+      return true;
+    } else {
+      this._hideInputError(input);
+      return false;
     }
+  };
 
-    _showInputError = (input, errorMessage) => {
-        const errorElement = this._formElement.querySelector(`.${input.id}-error`);
+  // Проверяем, есть ли невалидные инпуты
+  _hasInvalidInput = (inputList) => {
+    return inputList.some((input) => {
+      return !input.validity.valid;
+    });
+  };
 
-        input.classList.add(this._form.inputErrorClass);
-        errorElement.textContent = errorMessage;
+  // Активируем и дизактивируем кнопку отправки формы
+  _toggleButtonDisabled = (inputList, buttonElement) => {
+    // Проверяем инпуты на валидность
+    if (this._hasInvalidInput(inputList)) {
+      buttonElement.classList.add(this._form.inactiveButtonClass);
+      buttonElement.setAttribute("disabled", "");
+    } else {
+      buttonElement.classList.remove(this._form.inactiveButtonClass);
+      buttonElement.removeAttribute("disabled");
     }
+  };
 
-    _hideInputError = (input) => {
-        const errorElement = this._formElement.querySelector(`.${input.id}-error`);
+  // Прикрепление обработчика к инпутам
+  _setEventListeners = () => {
+    const inputList = Array.from(
+      this._formElement.querySelectorAll(this._form.inputSelector)
+    );
 
-        input.classList.remove(this._form.inputErrorClass);
-        errorElement.textContent = "";
-    }
+    const buttonElement = this._formElement.querySelector(
+      this._form.submitButtonSelector
+    ); //Кнопка формы
+    this._toggleButtonDisabled(inputList, buttonElement);
 
-    _checkValid = (input) => {
-        if (!input.validity.valid) {
-            this._showInputError(input, input.validationMessage);
-            return true;
-          } else {
-            this._hideInputError(input);
-            return false;
-          }
-    } 
+    inputList.forEach((input) => {
+      input.addEventListener("input", () => {
+        this._toggleButtonDisabled(inputList, buttonElement);
+        this._checkValid(input);
+      });
+    });
+  };
 
-    _setEventListeners = () => {
-        const inputList = Array.from(
-            this._formElement.querySelectorAll(this._form.inputSelector)
-        );
+  // Вызываем валидацию
+  enableValidation() {
+    this._formElement.addEventListener("submit", function (event) {
+      event.preventDefault();
+    });
 
-        inputList.forEach((input) => {
-            input.addEventListener('input', () => {
-                this._checkValid(input);
-            })
-        });
-    }
-
-    enableValidation() {
-        this._formElement.addEventListener("submit", function (event) {
-            event.preventDefault();
-        });
-
-        this._setEventListeners();
-    }
+    this._setEventListeners();
+  }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -79,6 +79,9 @@ const btnClosePicture = popupPicture.querySelector(
   ".popup__close-btn_name_picture"
 );
 
+// Находим все формы на странице
+const formList = Array.from(document.querySelectorAll(".form")); // Находим все формы на странице
+
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
 
@@ -106,7 +109,7 @@ function openPopup(popup) {
 }
 
 function renderPhoto(photo) {
-  const photoCard = new Card(photo.name, photo.link, '.photo-template'); // Создаем карточку
+  const photoCard = new Card(photo.name, photo.link, ".photo-template"); // Создаем карточку
 
   photoList.prepend(photoCard.createCard()); // Добавляем в разметку
 }
@@ -204,133 +207,17 @@ btnClosePicture.addEventListener("click", () => {
 
 popupPicture.addEventListener("click", closePopupOverlay); // Закрываем попап просмотра фотографии при клике на оверлей
 
-export { pictureTitle, pictureImg, popupPicture, openPopup }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Показываем текст ошибки
-function showInputError(form, formElement, input, errorMessage) {
-  const errorElement = formElement.querySelector(`.${input.id}-error`);
-
-  input.classList.add(form.inputErrorClass);
-  errorElement.textContent = errorMessage;
-}
-
-// Убираем текст ошибки
-function hideInputError(form, formElement, input) {
-  const errorElement = formElement.querySelector(`.${input.id}-error`);
-
-  input.classList.remove(form.inputErrorClass);
-  errorElement.textContent = "";
-}
-
-// Проверяем инпуты на валидность
-function checkValid(form, formElement, input) {
-  if (!input.validity.valid) {
-    showInputError(form, formElement, input, input.validationMessage);
-    return true;
-  } else {
-    hideInputError(form, formElement, input);
-    return false;
-  }
-}
-
-// Проверяем, есть ли невалидные инпуты
-function hasInvalidInput(inputList) {
-  return inputList.some((input) => {
-    return !input.validity.valid;
-  });
-}
-
-// Создаем функцию активации и дизактивации кнопки отправки формы
-function toggleButtonDisabled(form, inputList, buttonElement) {
-  // Проверяем инпуты на валидность
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add(form.inactiveButtonClass);
-    buttonElement.setAttribute("disabled", "");
-  } else {
-    buttonElement.classList.remove(form.inactiveButtonClass);
-    buttonElement.removeAttribute("disabled");
-  }
-}
-
-// Создаем функцию прикрепления обработчика к инпутам
-function setEventListeners(form, formElement) {
-  const inputList = Array.from(
-    formElement.querySelectorAll(form.inputSelector)
-  ); // Инпуты формы
-  const buttonElement = formElement.querySelector(form.submitButtonSelector); //Кнопка формы
-  toggleButtonDisabled(form, inputList, buttonElement);
-
-  inputList.forEach((input) => {
-    input.addEventListener("input", function () {
-      toggleButtonDisabled(form, inputList, buttonElement);
-      checkValid(form, formElement, input);
-    });
-  });
-}
-
-// Создаем функцию вызова валидации
-function enableValidation(form) {
-  const formElement = page.querySelector(form.formSelector); // Переданная форма
-
-  formElement.addEventListener("submit", function (event) {
-    event.preventDefault();
-  });
-
-  setEventListeners(form, formElement);
-}
-
-// Включаем валидацию формы редактирования
-enableValidation({
-  formSelector: ".form_edit",
-  inputSelector: ".form__text",
-  submitButtonSelector: ".form__btn",
-  inactiveButtonClass: "form__btn_disabled",
-  inputErrorClass: "form__text_invalid",
-});
-
-// Включаем валидацию формы добавления фото
-/*enableValidation({
-  formSelector: ".form_add-photo",
-  inputSelector: ".form__text",
-  submitButtonSelector: ".form__btn",
-  inactiveButtonClass: "form__btn_disabled",
-  inputErrorClass: "form__text_invalid",
-});*/
-
-const objFormElements = {
+// Элементы форм
+const formElements = {
   inputSelector: ".form__text",
   submitButtonSelector: ".form__btn",
   inactiveButtonClass: "form__btn_disabled",
   inputErrorClass: "form__text_invalid",
 };
 
-new FormValidator(objFormElements, formElementAddPhoto).enableValidation();
+// Подключаем валидацию ко всем формам на странице
+formList.forEach((formElement) => {
+  new FormValidator(formElements, formElement).enableValidation();
+});
 
-export {toggleButtonDisabled, checkValid};
+export { pictureTitle, pictureImg, popupPicture, openPopup };
